0a2f205dd4a2b4ad54a64e61d055e34033123e5a
chris bailey <chris.bailey@amazee.com>
2017-11-15 17:33:56 +0200

    fixed bug

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ if (process.env.NODE_ENV === 'production') {
 
 	// Express will serve the index.html file as default route
 	const path = require('path');
-	app.get('*' (req, res) => {
+	app.get('*', (req, res) => {
 		res.sendFile(path,resolve(__dirname,'client', 'build', 'index.html'));
 	});
 }
