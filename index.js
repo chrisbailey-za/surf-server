7e780d583a5ddab6affab0f9960e2f5a1a243b60
chris-amazee <kommetjiechris@yahoo.com>
2018-01-05 09:16:51 +0200

    added tide call on startup

--- index.js
+++ index.js
@@ -50,6 +50,7 @@ if (process.env.NODE_ENV === 'production') {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
+ tideCall();
 
 	var tideCron = new CronJob('0 0 0 * * *', () => {
 		 tideCall();
